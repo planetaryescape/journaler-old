a684fe14c94612d492a9af992067a9ce81236323
Bhekani Khumalo <bhekanik@gmail.com>
2024-05-16 11:13:19 +0100

    feat: ...

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,6 +1,5 @@
 import { Resend } from "resend";
 
-const apiKey = (process.env.RESEND_API_KEY as string) ?? "";
-console.log("apiKey:", apiKey);
+const apiKey = (process.env.RESEND_API_KEY as string) ?? "re_123";
 
 export const resend = new Resend(apiKey);
