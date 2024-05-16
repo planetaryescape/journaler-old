c38c2939e5fc223206fdba2e206ee43e1cad7526
Bhekani Khumalo <bhekanik@gmail.com>
2024-05-16 10:33:48 +0100

    feat: Dont show hero and features to logged in users

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -2,13 +2,15 @@ import { ContactSection } from "@/components/contact-section";
 import { CtaWithFeatures } from "@/components/cta-with-features";
 import { Hero } from "@/components/hero";
 import { Prompts } from "@/components/prompts";
+import { auth } from "@clerk/nextjs/server";
 
 const HomePage = () => {
+  const { userId } = auth();
   return (
     <div>
-      <Hero />
+      {!userId && <Hero />}
       <Prompts title="All Time Top Prompts" limit={10} />
-      <CtaWithFeatures />
+      {!userId && <CtaWithFeatures />}
       <ContactSection />
     </div>
   );
