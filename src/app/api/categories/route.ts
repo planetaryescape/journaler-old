c061e6bec519edf4df9becf0bafc566c17040263
Bhekani Khumalo <bhekanik@gmail.com>
2024-05-19 00:02:37 +0100

    chore: Get list of prompts without checking auth

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -2,27 +2,18 @@ import { db } from "@/db";
 import { prompts } from "@/db/schema/prompts";
 import { createDefaultApiRouteContext } from "@/lib/createDefaultApiRouteContext";
 import { logger } from "@/lib/logger";
-import { getAuth } from "@clerk/nextjs/server";
 import { and, gt } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const { userId } = getAuth(request);
-
   const { searchParams } = new URL(request.url);
   const earliest = searchParams.get("earliest") ?? null;
 
   const context = {
     ...createDefaultApiRouteContext(request),
     tracePath: "/api/categories",
-    userId,
     earliest,
   };
-
-  if (!userId) {
-    logger.error(context, "Unauthorized");
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
 
   try {
     logger.info({ ...context }, "Getting categories");
--- src/app/api/prompts/route.ts
+++ src/app/api/prompts/route.ts
@@ -3,13 +3,10 @@ import { interactions } from "@/db/schema/interactions";
 import { prompts } from "@/db/schema/prompts";
 import { createDefaultApiRouteContext } from "@/lib/createDefaultApiRouteContext";
 import { logger } from "@/lib/logger";
-import { getAuth } from "@clerk/nextjs/server";
 import { and, eq, gt } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const { userId } = getAuth(request);
-
   const { searchParams } = new URL(request.url);
   const earliest = searchParams.get("earliest") ?? null;
   const categoryId = searchParams.get("categoryId") ?? null;
@@ -17,15 +14,9 @@ export async function GET(request: NextRequest) {
   const context = {
     ...createDefaultApiRouteContext(request),
     tracePath: "/api/prompts",
-    userId,
     earliest,
     categoryId,
   };
-
-  if (!userId) {
-    logger.error(context, "Unauthorized");
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
 
   try {
     logger.info({ ...context }, "Getting prompts");
